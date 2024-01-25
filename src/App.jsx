7c249bda0bfbe141139dc0e34f2b788bbfbaf504
carodriguezp <crpzgz92@gmail.com>
2024-01-25 18:58:24 +0100

    new branch created and exercise 315 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <main className="main">
+        <h1>#cc212a</h1>
+        <h2>fire engine red</h2>
+      </main>
+
+      <footer className="footer">
+        <div className="footer__div--line"></div>
+        <div className="footer__div">
+          <p className="footer__div--date">© 2021 fer.</p>
+          <p className="footer__div--button">r</p>
+
+        </div>
+
+      </footer>
     </div>
   );
 }
