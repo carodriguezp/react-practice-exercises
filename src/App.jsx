0d4abee7661dd9219e4f2eaf72e534251595660c
carodriguezp <crpzgz92@gmail.com>
2024-01-25 19:39:31 +0100

    exercise 317 done and branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.scss'
+import manati from './images/manati.jpg'
 
 function App() {
   return (
     <div>
-
+      <img src={manati} title="manati" alt="foto de manati" />
     </div>
   );
 }
