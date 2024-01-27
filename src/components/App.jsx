6c03343b1cbd824348dc38c291e94d4ead8b3865
carodriguezp <crpzgz92@gmail.com>
2024-01-27 13:57:53 +0100

    dark mode exercise done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
 import '../style/App.scss'
 
+import { useState } from 'react'; //para que use lo de más abajo
+
 function App() {
-  const user = "Carolina";
+  const [colorMode, setMode] = useState(''); //para que setee el valor inicial de colorMode en el div
+
+  const handleDarkMode = () => {
+    //condicional
+    if (colorMode === 'dark') {
+      setMode('light');
+    } else {
+      setMode('dark');
+    }
+
+  };
+
 
   return (
-    <>
-      <h1>Mi web {user} </h1>
-    </>
+    <div className={colorMode}>
+      <button onClick={handleDarkMode}>Des/activar el dark mode</button>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum perspiciatis, fugiat excepturi perferendis harum libero aut. Debitis deleniti odio velit commodi laboriosam mollitia, deserunt architecto, culpa harum ut hic vel!</p>
+
+    </div>
   );
 }
 
