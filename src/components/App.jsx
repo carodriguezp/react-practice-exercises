d6bbc67c14e15267d8f9b1e7fd6d75d023d0b989
carodriguezp <crpzgz92@gmail.com>
2024-01-27 13:08:04 +0100

    exercise started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,48 @@
 import '../style/App.scss'
 
-function App() {
-  const user = "Carolina";
+import { useState } from "react";
 
+// Creamos la función o componente App. Esta vez lo creamos con una función arrow.
+const App = () => {
+
+  let [number, setNumber] = useState('0');
+
+  const handleButtonSum = (ev) => {
+    ev.preventDefault(); // para parar la acción por defecto del evento
+    console.log('El evento lo ha lanzado la etiqueta: ', ev.target, 'que es', number);
+    let number = parseInt(number) + 1;
+    setNumber(ev.target.value)
+  };
+
+  const handleButtonRest = (ev) => {
+    ev.preventDefault(); // para parar la acción por defecto del evento
+    console.log('El evento lo ha lanzado la etiqueta: ', ev.target);
+    let number = parseInt(number) - 1;
+    setNumber(ev.target.value)
+  };
+
+  const handleButtonReset = (ev) => {
+    ev.preventDefault(); // para parar la acción por defecto del evento
+
+    console.log('El evento lo ha lanzado la etiqueta: ', ev.target);
+    number = 0;
+    setNumber(ev.target.value)
+  };
+
+  // Retornamos todo el código HTML que queremos que React pinte en la página.
   return (
-    <>
-      <h1>Mi web {user} </h1>
-    </>
-  );
-}
+    <div>
+      <h1>El contador</h1>
 
-export default App
+      <p>Contador: <strong>{number}</strong> </p>
+
+      <button onClick={handleButtonSum}>Suma</button>
+      <button onClick={handleButtonRest}>Resta</button>
+      <button onClick={handleButtonReset}>Reset</button>
+
+
+    </div>
+  );
+};
+
+export default App;
