4052e4bf7c14eaa902ca10535a844b90b0215041
carodriguezp <crpzgz92@gmail.com>
2024-01-29 18:17:46 +0100

    exercise finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,46 @@
 import '../style/App.scss'
+import { useState } from 'react';
 
 function App() {
-  const user = "Carolina";
+
+  const [listState, setState] = useState('hidden');
+
+  const handleClick = () => {
+
+    listState === 'shown' ? setState('hidden') : setState('shown')
+
+    // if (listState === 'shown') {
+    //   setState('hidden')
+    // } else {
+    //   setState('shown')
+    // }
+
+  }
 
   return (
     <>
-      <h1>Mi web {user} </h1>
+      <header className='header'>
+        <nav className='header-nav'>
+          <i className="fa-solid fa-bars" onClick={handleClick}></i>
+
+          <div className='header-nav'>
+            <i className="fa-solid fa-heart"></i>
+            <p>Mi web</p>
+          </div>
+
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </nav>
+      </header>
+
+      <div className='container'>
+        <ul className={`list  ${listState}`}>
+          <li><i className="fa-solid fa-heart"></i> <p>Elemento de la lista</p></li>
+          <li><i className="fa-solid fa-heart"></i> <p>Elemento de la lista</p></li>
+          <li><i className="fa-solid fa-heart"></i> <p>Elemento de la lista</p></li>
+          <li><i className="fa-solid fa-heart"></i> <p>Elemento de la lista</p></li>
+          <li><i className="fa-solid fa-heart"></i> <p>Elemento de la lista</p></li>
+        </ul>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import App from './components/App.jsx'
+import './style/App.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
