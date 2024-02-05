987d2dafb555c9dae2738d734b1545893b27c9b3
carodriguezp <crpzgz92@gmail.com>
2024-02-05 17:44:58 +0100

    calculator done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,90 @@
+import { useState } from 'react';
 import '../style/App.scss';
-// import { useState } from 'react';
+
 
 function App() {
-  const user = "Carolina";
+
+  const [numberA, setNumberA] = useState(0);
+  const [numberB, setNumberB] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [operation, setOperation] = useState("Suma");
+
+  const handleNumberA = (ev) => {
+    setNumberA(ev.target.value);
+  };
+
+  const handleNumberB = (ev) => {
+    setNumberB(ev.target.value);
+  };
+
+
+  const handleOperation = (event) => {
+
+    setOperation(event.target.value)
+
+    if (event.target.value === "Suma") {
+
+      setTotal(parseInt(numberA) + parseInt(numberB));
+
+    } else if (event.target.value === "Resta") {
+
+      setTotal(parseInt(numberA) - parseInt(numberB));
+
+    } else if (event.target.value === "Multiplicación") {
+
+      setTotal(parseInt(numberA) * parseInt(numberB));
+
+    } else if (event.target.value === "División") {
+
+      setTotal(parseInt(numberA) / parseInt(numberB));
+
+    }
+  }
 
   return (
-    <>
-      <h1>Mi web {user} </h1>
-    </>
+    <div>
+      <h1>La calculadora:</h1>
+      <form>
+        <label>
+          Escribe un número:
+          <input
+            type="number"
+            name="name"
+            onChange={handleNumberA}
+            value={numberA}
+          />
+        </label>
+        <label>
+          Escribe otro número:
+          <input
+            type="number"
+            name="email"
+            onChange={handleNumberB}
+            value={numberB}
+          />
+        </label>
+
+        <label htmlFor="operation">
+          ¿Qué operación matemática quieres hacer?
+        </label>
+        <select
+          className="input-select"
+          name="operation"
+          id="operation"
+          value={operation}
+          onChange={handleOperation}
+        >
+          <option value="Suma">Suma</option>
+          <option value="Resta">Resta</option>
+          <option value="Multiplicación">Multiplicación</option>
+          <option value="División">División</option>
+
+        </select>
+      </form>
+      <p>
+        El resultado de la <strong>{operation}</strong> entre <strong>{numberA}</strong> y <strong>{numberB}</strong> es <strong> {total}</strong>.
+      </p>
+    </div>
   );
 }
 
