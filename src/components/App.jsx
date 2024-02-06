6fc9494aaf8a4ad347f43f2c8c45e777fc88b291
carodriguezp <crpzgz92@gmail.com>
2024-02-06 16:09:53 +0100

    render done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,72 @@
-import '../style/App.scss';
-// import { useState } from 'react';
+import { useEffect, useState } from "react";
+import callToApi from "../services/api";
 
-function App() {
-  const user = "Carolina";
+const App = () => {
+  // Estados
+
+  // Creamos characters para guardar los personajes respondidos por la API, por eso es un array vacío
+  const [shows, setShows] = useState([]);
+  // Creamos searchName para gestionar el input de búsqueda, por eso es un string vacío
+  const [searchName, setSearchName] = useState("");
+
+  // useEffect--lo hacemos porque queremos controlar un bloque de codigo cuando cambie una variable de estado
+
+  useEffect(() => {
+    // Aquí podemos poner código JS, por ejemplo podríamos llamar a callToApi dentro de un if
+
+    // Llamamos al API pasando por parámetros el searchName
+    callToApi(searchName).then((response) => {
+      // Cuando el API responde guardamos los personajes en el estado
+      setShows(response);
+    });
+    // Este useEffect depende de searchName, por eso ponemos [searchName]
+    // Cuando la usuaria cambia el searchName este useEffect se vuelve a ejecutar porque necesitamos llamar otra vez a la API para obtener nuevos datos
+  }, [searchName]);
+
+  // Eventos
+
+  const handleSearchName = (ev) => {
+    // Cuando la usuaria cambia el input guardamos su valor en el estado
+    setSearchName(ev.target.value);
+  };
+
+  // Renderizado
+
+  const renderShows = () => {
+    // Pintamos el listado de personajes respondido por la API
+    return shows.map(({ score, name }, index) => {
+
+      // La API no nos devuelve un id para cada personaje, por eso usamos el index que nos da el map
+      return <li key={index}>
+
+        <p>Nombre: {name}</p>
+        <p>Rating: {score * 100}%</p>
+
+      </li>;
+    });
+  };
 
   return (
-    <>
-      <h1>Mi web {user} </h1>
-    </>
-  );
-}
+    <div>
+      <h1>Llamar a la API de Series:</h1>
 
-export default App
+      <form>
+        <label htmlFor="name">
+          Busca por el nombre de tu serie favorita:
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={searchName}
+          onChange={handleSearchName}
+        />
+      </form>
+
+      <h2>Series con el nombre: {searchName}</h2>
+      <ul>{renderShows()}</ul>
+    </div>
+  );
+};
+
+export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,18 @@
-const callToApi = () => {
-
+const callToApi = (searchName) => {
     // Llamamos a la API
-    return fetch("")
+    return fetch(`https://api.tvmaze.com/search/shows?q=${searchName}`) // Este 5 es el id de Leia Skywalker
         .then((response) => response.json())
         .then((response) => {
-            // Cuando responde la API podemos limpiar los datos aquí
-            const result = {
 
-            };
+            // Cuando responde la API podemos limpiar los datos aquí
+            const result = response.map(({ score, show }) => {
+
+                {/*como show esta entre llaves accedo directamente a la propiedad show del objeto */ }
+                return {
+                    score,
+                    name: show.name,
+                };
+            });
             return result;
         });
 };
